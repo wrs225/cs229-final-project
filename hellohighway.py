c01d36ef4223e90e32629e390f782585751aa820
Will <wrs225@cornell.edu>
2023-10-15 17:15:28 -0700

    Trained model!

--- hellohighway.py
+++ hellohighway.py
@@ -1,13 +1,29 @@
 import gymnasium as gym
-from matplotlib import pyplot as plt
+import highway_env
+from stable_baselines3 import DQN
 
+env = gym.make("highway-fast-v0", render_mode='rgb_array')
+model = DQN('MlpPolicy', env,
+              policy_kwargs=dict(net_arch=[256, 256]),
+              learning_rate=5e-4,
+              buffer_size=15000,
+              learning_starts=200,
+              batch_size=32,
+              gamma=0.8,
+              train_freq=1,
+              gradient_steps=1,
+              target_update_interval=50,
+              verbose=1,
+              tensorboard_log="highway_dqn/")
+#model.learn(int(2e4))
+#model.save("highway_dqn/model")
 
-env = gym.make('highway-v0', render_mode='rgb_array')
-env.reset()
-for _ in range(40):
-    action = env.action_type.actions_indexes["IDLE"]
+# Load and test saved model
+model = DQN.load("highway_dqn/model")
+while True:
+  done = truncated = False
+  obs, info = env.reset()
+  while not (done or truncated):
+    action, _states = model.predict(obs, deterministic=True)
     obs, reward, done, truncated, info = env.step(action)
     env.render()
-
-plt.imshow(env.render())
-plt.show()
