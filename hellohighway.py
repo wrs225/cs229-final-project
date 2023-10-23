e336ae0d4db9ff49ae9ef6ac736c03eb5b269813
brandonkim-1301 <brandonkim1301@gmail.com>
2023-10-23 00:19:52 -0700

    trained ppo merge, updated path vars

--- hellohighway.py
+++ hellohighway.py
@@ -4,11 +4,13 @@ import highway_env
 from stable_baselines3 import DQN, PPO
 from stable_baselines3.common.logger import configure
 
-# Sets up logger
-data_path = "./highway_ppo/log/"
-logger = configure(data_path, ["stdout", "csv", "tensorboard"])
+# Sets up logger (ref notes in drive for link)
+model_path = "./highway_ppo_merge/"
+log_path = model_path + "log/"
+save_path = model_path + "model/"
+logger = configure(log_path, ["stdout", "csv", "tensorboard"])
 
-env = gym.make("highway-fast-v0", render_mode='rgb_array')
+env = gym.make("merge-v0", render_mode='rgb_array')
 # model = DQN('MlpPolicy', env,
 #               policy_kwargs=dict(net_arch=[256, 256]),
 #               learning_rate=5e-4,
@@ -29,13 +31,13 @@ model = PPO('MlpPolicy', env,
               gamma=0.8,
               verbose=1,
               stats_window_size=100,
-              tensorboard_log="highway_ppo/")
+              tensorboard_log=model_path)
 model.set_logger(logger)  #sets new logger, overwriting tensorboard_log config
 model.learn(int(2e4))
-model.save("highway_ppo/model")
+model.save(save_path)
 
 # Load and test saved model
-model = PPO.load("highway_ppo/model")
+model = PPO.load(save_path)
 while True:
   done = truncated = False
   obs, info = env.reset()
