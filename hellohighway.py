8cb1625b9b7e3bf7eadc0d2b2def77b928e9286e
brandonkim-1301 <brandonkim1301@gmail.com>
2023-10-23 01:42:21 -0700

    trained ppo intersection (_v0 better results)

--- hellohighway.py
+++ hellohighway.py
@@ -3,14 +3,18 @@ import gymnasium as gym
 import highway_env
 from stable_baselines3 import DQN, PPO
 from stable_baselines3.common.logger import configure
+import time
 
-# Sets up logger (ref notes in drive for link)
-model_path = "./highway_ppo_roundabout/" #just update this between models/scenarios
+# Sets up logger (ref notes in drive for more info)
+#   Update next two lines between models/scenarios
+scenario = "intersection-v0"
+model_path = "./highway_ppo_intersection_v0/" 
+
 log_path = model_path + "log/"
 save_path = model_path + "model/"
 logger = configure(log_path, ["stdout", "csv", "tensorboard"])
 
-env = gym.make("roundabout-v0", render_mode='rgb_array')
+env = gym.make(scenario, render_mode='rgb_array')
 # model = DQN('MlpPolicy', env,
 #               policy_kwargs=dict(net_arch=[256, 256]),
 #               learning_rate=5e-4,
@@ -33,8 +37,10 @@ model = PPO('MlpPolicy', env,
               stats_window_size=100,
               tensorboard_log=model_path)
 model.set_logger(logger)  #sets new logger, overwriting tensorboard_log config
+start_time = time.time()
 model.learn(int(2e4))
 model.save(save_path)
+print("------- %.2f seconds to train -------" % (time.time() - start_time))
 
 # Load and test saved model
 model = PPO.load(save_path)
